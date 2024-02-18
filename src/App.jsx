2c20c2fea0b8dc9bd6e0bb0acbbb0572b9bd5099
tictools <jaumevillarreal@gmail.com>
2024-02-18 19:19:44 +0100

    feat: add state management + business logic + tests + responsive styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,130 @@
-function App() {
+import { useState } from "react";
+import { Anecdote, AnecdoteCard } from "./anecdotes";
+import { anecdotes } from "./anecdotes/data";
+import { getMostVotedAnecdoteData } from "./anecdotes/helpers";
+import { initialVoteState } from "./anecdotes/initialVoteState";
+import { Statistics } from "./feedback";
+import { statisticsData } from "./feedback/data";
+import {
+  calculatePositiveAverage,
+  calculateTotalAverage,
+  shouldRenderStatistics,
+} from "./feedback/helpers";
+import { Button, ButtonSet, MainHeader, SectionHeader } from "./shared";
+
+const App = () => {
+  // feedback state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // anecdotes state
+  const [selected, setSelected] = useState(0);
+  const [voted, setVoted] = useState(initialVoteState);
+
+  // feedback handlers
+  const totalFeedback = good + neutral + bad;
+
+  const handleIncreaseGood = () => {
+    setGood((prevTotal) => prevTotal + 1);
+  };
+
+  const handleIncreaseNeutral = () => {
+    setNeutral((prevTotal) => prevTotal + 1);
+  };
+
+  const handleIncreaseBad = () => {
+    setBad((prevTotal) => prevTotal + 1);
+  };
+
+  // feedback data
+  const averageStatistic = calculateTotalAverage({ good, bad, totalFeedback });
+  const positiveAverage = calculatePositiveAverage({ good, totalFeedback });
+
+  const statistics = [
+    statisticsData.goodStatistic(good),
+    statisticsData.neutralStatistic(neutral),
+    statisticsData.badStatistic(bad),
+    statisticsData.allStatistic(totalFeedback),
+    statisticsData.totalAverageStatistic(averageStatistic),
+    statisticsData.positiveAverageStatistic(positiveAverage),
+  ];
+
+  // anecdotes handlers
+  const handleRandomAnecdote = () => {
+    const anecdotesRange = anecdotes.length;
+    const index = Math.floor(Math.random() * anecdotesRange);
+
+    setSelected(index);
+  };
+
+  const handleUpdateVotes = () => {
+    setVoted((prevVotes) => {
+      const nextVotes = [...prevVotes];
+      nextVotes[selected] += 1;
+
+      return nextVotes;
+    });
+  };
+
+  // anecdotes data
+  const { mostVotedAnecdote, maxVoteValue } = getMostVotedAnecdoteData({
+    votesCollection: voted,
+    anecdotesCollection: anecdotes,
+  });
+
   return (
     <>
-      <div>Hello world</div>
+      <MainHeader text="React state management" />
+      <main className="main">
+        <section className="section">
+          <SectionHeader text="give feedback" />
+          <ButtonSet>
+            <Button
+              label="good"
+              handleUpdate={handleIncreaseGood}
+              level="success"
+            />
+            <Button
+              label="neutral"
+              handleUpdate={handleIncreaseNeutral}
+              level="secondary"
+            />
+            <Button
+              label="bad"
+              handleUpdate={handleIncreaseBad}
+              level="danger"
+            />
+          </ButtonSet>
+          <Statistics
+            statistics={statistics}
+            shouldRenderStatistics={shouldRenderStatistics({ totalFeedback })}
+          />
+        </section>
+        <section className="section">
+          <SectionHeader text="anecdotes" />
+          <AnecdoteCard title="Anecdote of the day">
+            <Anecdote content={anecdotes[selected]} votes={voted[selected]} />
+            <ButtonSet>
+              <Button
+                label="Vote"
+                handleUpdate={handleUpdateVotes}
+                level="success"
+              />
+              <Button
+                label="Next"
+                handleUpdate={handleRandomAnecdote}
+                level="secondary"
+              />
+            </ButtonSet>
+          </AnecdoteCard>
+          <AnecdoteCard title="Anecdote with most votes">
+            <Anecdote content={mostVotedAnecdote} votes={maxVoteValue} />
+          </AnecdoteCard>
+        </section>
+      </main>
     </>
   );
-}
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
+import "./sanitize.css";
+import "./utilities.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
